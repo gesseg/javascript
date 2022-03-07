d2d2b9bca015ce6c89cc15e5caac6bd83e435a2b
Gesse Pereira <gessegpereira@gmail.com>
2022-03-07 13:55:24 -0300

    first commit

---
+++ string.js
@@ -0,0 +1,9 @@
+const texto1 = "Olá, mundo!"; 
+const texto2 = 'O Léo disse: "pode ser!"';
+const cifrao = '\u0024'
+const hiragana = '\u3041'
+
+console.log(hiragana);
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
